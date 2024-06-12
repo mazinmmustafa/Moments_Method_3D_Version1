293da7ee44f92e62c65b625fb7492ffefda6967b
Mazin Mustafa <mazin.mustafa@outlook.com>
2024-06-11 22:57:05 -0500

    update

---
+++ mesh/generate_mesh.py
@@ -0,0 +1,15 @@
+from engine import mesh
+import os
+
+is_view = False
+
+# export .brep files from FreeCAD
+filename_FreeCAD = "mesh/FreeCAD/sphere.brep"
+filename_Mesh = "mesh/shape.vtk"
+
+mesh.call_gmsh(filename_FreeCAD, 3, 0.08)
+if is_view:
+    os.system(f"gmsh {filename_Mesh}")
+    pass
+elements = mesh.read_mesh(filename_Mesh)
+mesh.write_mesh(elements)
